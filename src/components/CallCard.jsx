4443043c0c87add07c6dc8ddb2bfc1beadf254c7
biancahpp <bianca_procopio@hotmail.com>
2021-09-21 20:26:17 -0600

    feat: basic style for activity page

--- src/components/CallCard.jsx
+++ src/components/CallCard.jsx
@@ -4,14 +4,14 @@ import { FiPhoneIncoming, FiPhoneOutgoing } from 'react-icons/fi'
 export default function CallCard({call}) {
   return (
     <div className="call-card">
-      <div>
-        { call.direction === 'inbound'? <FiPhoneIncoming /> : <FiPhoneOutgoing />}
+      <div className="call-card-left">
+        { call.direction === 'inbound'? <FiPhoneIncoming className="call-card-icon"/> : <FiPhoneOutgoing className="call-card-icon"/>}
+        <div>
+          <p className="call-card-name">{call.from}</p>
+          <p className="call-card-via">tried to call on {call.via}</p>
+        </div>
       </div>
-      <div>
-        <p>{call.from}</p>
-        <p>tried to call on {call.via}</p>
-      </div>
-      <div>
+      <div className="call-card-right">
         <span>
           {new Date(call.created_at).toLocaleString('default', {
             hour: '2-digit',
